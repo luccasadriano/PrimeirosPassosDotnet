bfd562908bd831c8c153a32e47f5036781a433fa
kaizokufox <luccas.karate.2012@gmail.com>
2020-03-17 00:03:35 -0300

    Add Estrutura do Programa

---
+++ 01EstruturaDoPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Pilhar = new Pilha();
+
+            Pilhar.Empilhar(1);
+            Pilhar.Empilhar(5);
+            Pilhar.Empilhar(10);
+            Console.WriteLine(Pilhar.Desempilhar());
+            Console.WriteLine(Pilhar.Desempilhar());
+            Console.WriteLine(Pilhar.Desempilhar());
+            Console.WriteLine(Pilhar.Desempilhar()); //acionando a exception
+        }
+    }
+}
